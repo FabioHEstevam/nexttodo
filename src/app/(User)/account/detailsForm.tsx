87072c463089deb0e1c460f120d2a4340c9c3d8a
fabioestevam <fabio_henrique_estevam@hotmail.com>
2023-12-30 15:54:05 -0300

    Validação do formulario de atualização dos dados do usuário

--- src/app/(User)/account/detailsForm.tsx
+++ src/app/(User)/account/detailsForm.tsx
@@ -2,32 +2,63 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { toast } from "@/components/ui/use-toast";
 import axios from "axios";
 import { useState } from "react";
 import { useSession, getSession } from "next-auth/react"
+import { unknown, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { useRouter } from "next/navigation";
-
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 type Props = {}
 
+const MAX_FILE_SIZE = 2000000;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+const UpdateSchema = z.object({
+
+    username: z.union(
+        [
+            z.string().min(3, { message: "Username must be at least 3 characters long" }),
+            z.string().length(0)
+        ])
+        .optional()
+        .transform(e => e === "" ? undefined : e),
+
+    image: z.any()
+        .refine((file) =>
+            file?.size <= MAX_FILE_SIZE, `Max image size is 2MB.`)
+        .refine((file) =>
+            ACCEPTED_IMAGE_TYPES.includes(file?.type),
+            "Only .jpg, .jpeg, .png and .webp formats are supported."
+        )
+        .optional()
+})
 
 function DetailsForm(props: Props) {
 
-    const { data: Session, status, update } = useSession()
+    const { data: Session, update } = useSession()
 
     const [image, setImage] = useState<File>();
-    const [username, setUserName] = useState(Session?.user?.name)
 
-    const router = useRouter()
+    const username = Session?.user?.name as string;
 
-    async function onSubmit() {
+    const form = useForm<z.infer<typeof UpdateSchema>>({
+        resolver: zodResolver(UpdateSchema),
+        defaultValues: {
+            username: '',
+        }
+    })
+
+    const { ref, ...field } = form.register("image")
+
+    async function onSubmit(values: z.infer<typeof UpdateSchema>) {
 
         const formData = new FormData();
+
+        const username = values.username;
+        const image = values.image;
 
         if (image) {
             formData.append('image', image);;
@@ -39,22 +70,23 @@ function DetailsForm(props: Props) {
             }
         }
 
-        try {
-            const response = await axios.post('/api/update', formData).then(() => {
-                update().then(async () => {
-                    toast({
-                        description: "User data updated"
-                    })
-                     setTimeout(()=>location.reload(),2000);
-                });
-            })
-        } catch (error) {
-            console.log(error)
-            toast({
-                description: "Something went wrong"
-            })
+        if (username && image) {
+            try {
+                const response = await axios.post('/api/update', formData).then(() => {
+                    update().then(async () => {
+                        toast({
+                            description: "User data updated"
+                        })
+                        setTimeout(() => location.reload(), 2000);
+                    });
+                })
+            } catch (error) {
+                console.log(error)
+                toast({
+                    description: "Something went wrong"
+                })
+            }
         }
-
     }
 
     return (
@@ -66,20 +98,43 @@ function DetailsForm(props: Props) {
                 </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-                <div className="space-y-1">
-                    <Label htmlFor="username">Profile name</Label>
-                    <Input id="username" defaultValue={Session?.user?.name as string} onChange={(e) => setUserName(e.target.value)} />
-                </div>
-
-                <div className="space-y-1">
-                    <Label htmlFor="image">Profile picture</Label>
-                    <Input id="image" type="file" onChange={(e) => e.target.files && setImage(e.target.files[0])} />
-                </div>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>User name:</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder={username} {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                        <FormField
+                            control={form.control}
+                            name="image"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Profile picture:</FormLabel>
+                                    <FormControl>
+                                        <Input type="file"
+                                            onChange={(e) => e.target.files && form.setValue('image', e.target.files[0])}
+                                            name={field.name}
+                                            onBlur={field.onBlur}
+                                            ref={field.ref}
+                                            disabled={field.disabled} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                        <div className="flex flex-row justify-center w-full">
+                            <Button typeof="submit" className="">Submit</Button>
+                        </div>
+                    </form>
+                </Form>
             </CardContent>
-            <CardFooter>
-                <Button onClick={() => onSubmit()}>Save changes</Button>
-            </CardFooter>
         </Card>
     )
 }
--- src/app/(User)/account/passwordForm.tsx
+++ src/app/(User)/account/passwordForm.tsx
@@ -2,9 +2,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import DetailsForm from "./detailsForm";
 import { useToast } from "@/components/ui/use-toast";
 import { useSession, signOut } from 'next-auth/react'
 import { useForm } from "react-hook-form";
